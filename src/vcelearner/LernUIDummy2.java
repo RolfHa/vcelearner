1d5838a08f6a85720de0b4f780bb5c366839aefd
J.Weidehaas <J.Weidehaas@5-6--4.Bildung.zz>
2016-05-31 14:01:30 +0200

    UI Layout Funktional

---
+++ src/vcelearner/LernUIDummy2.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vcelearner;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author J.Bleich
+ */
+public class LernUIDummy2 {
+
+    public static void main(String[] args) {
+        ArrayList<LernKarte> lKs = LernKarte.getAll();
+        BenutzerSitzung session = new BenutzerSitzung(20,
+                new Benutzer("Petra", "Panke"), lKs);
+/* abgeänderte Kopie aus UI-main */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(LernenUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(LernenUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(LernenUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(LernenUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LernenUI(session).setVisible(true); // hier konstruktor geändert
+            }
+        });
+/* ende abgeänderte kopie */
+    }
+}
